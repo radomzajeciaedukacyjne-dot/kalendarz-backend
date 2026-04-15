f5487d9518cf7ec18171711ad71ca2c1ea7b4ebf
radomzajeciaedukacyjne-dot <radom.zajecia.edukacyjne@gmail.com>
2026-04-15 13:07:01 +0200

    nowy

--- server.js
+++ server.js
@@ -76,6 +76,8 @@ Nadleśnictwo Radom
 });
 
 // 🔥 START SERWERA
-app.listen(3000, () => {
-  console.log("Serwer działa na http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Serwer działa na porcie " + PORT);
 });
